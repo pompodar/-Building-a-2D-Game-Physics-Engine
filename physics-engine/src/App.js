29d9b5d3d553f8db6ef3e475de5f932a9d734ddb
svyatoslavkachmar-cnic <Svyatoslav.Kachmar@centralnic.com>
2024-08-25 20:16:17 +0300

    refactor

--- physics-engine/src/App.js
+++ physics-engine/src/App.js
@@ -1,22 +1,19 @@
-import React, { useEffect, useRef, useCallback } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Vec2 from './Vec2';
 import Rectangle from './Rectangle';
-import Circle from './Circle';
 import CollisionInfo from './CollisionInfo';
+import KeyHandler from './KeyHandler';
 
 const App = () => {
-  const GRAVITY = new Vec2(0, 10);
-  let movement = true;
-
-  let positionalCorrectionFlag = true;
-  // number of relaxation iteration
-  let relaxationCount = 15;
-  // percentage of separation to project objects
-  let mPosCorrectionRate = 0.8;
-
   const canvasRef = useRef(null);
   const allObjectsRef = useRef([]);
   const gObjectNumRef = useRef(0);
+
+  const GRAVITY = new Vec2(0, 10);
+  const movement = true;
+  const positionalCorrectionFlag = true;
+  const relaxationCount = 15;
+  const mPosCorrectionRate = 0.8;
 
   const updateGameObjects = () => {
     const canvas = canvasRef.current;
@@ -26,92 +23,106 @@ const App = () => {
 
     context.clearRect(0, 0, width, height);
 
-    allObjectsRef.current.forEach(obj => {
+    allObjectsRef.current.forEach((obj) => {
       obj.update(canvas);
       obj.draw(context);
     });
 
-    var collisionInfo = new CollisionInfo();
+    const collisionInfo = new CollisionInfo();
 
-    // Функція для малювання інформації про зіткнення
-    var drawCollisionInfo = function (collisionInfo, context) {        
+    const drawCollisionInfo = (collisionInfo, context) => {
       context.beginPath();
       context.moveTo(collisionInfo.mStart.x, collisionInfo.mStart.y);
       context.lineTo(collisionInfo.mEnd.x, collisionInfo.mEnd.y);
       context.closePath();
-      context.strokeStyle = "green";
+      context.strokeStyle = 'green';
       context.stroke();
     };
 
-    const positionalCorrection = function (s1, s2, collisionInfo) {
+    const positionalCorrection = (s1, s2, collisionInfo) => {
       const s1InvMass = s1.mInvMass;
       const s2InvMass = s2.mInvMass;
-      const num = collisionInfo.getDepth() / (s1InvMass + s2InvMass) * mPosCorrectionRate;
+      const num =
+        (collisionInfo.getDepth() / (s1InvMass + s2InvMass)) *
+        mPosCorrectionRate;
       const correctionAmount = collisionInfo.getNormal().scale(num);
       s1.move(correctionAmount.scale(-s1InvMass));
       s2.move(correctionAmount.scale(s2InvMass));
     };
 
-    const resolveCollision = function (s1, s2, collisionInfo) {
-      if ((s1.mInvMass === 0) && (s2.mInvMass === 0)) return;
-        
-        // correct positions
-        if (positionalCorrectionFlag)
-        positionalCorrection(s1, s2, collisionInfo);
-        const n = collisionInfo.getNormal();
-        const v1 = s1.mVelocity;
-        const v2 = s2.mVelocity;
-        const relativeVelocity = v2.subtract(v1);
-        // Relative velocity in normal direction
-        const rVelocityInNormal = relativeVelocity.dot(n);
-        // if objects moving apart ignore
-        if (rVelocityInNormal > 0)
-        return;
-        // compute and apply response impulses for each object
-        const newRestituion = Math.min(s1.mRestitution, s2.mRestitution);
-        const newFriction = Math.min(s1.mFriction, s2.mFriction);
-        // Calc impulse scalar
-        let jN = -(1 + newRestituion) * rVelocityInNormal;
-        jN = jN / (s1.mInvMass + s2.mInvMass);
-        //impulse is in direction of normal ( from s1 to s2)
-        let impulse = n.scale(jN);
-        // impulse = F dt = m * v
-        // v = impulse / m
-        s1.mVelocity = s1.mVelocity.subtract(impulse.scale(s1.mInvMass));
-        s2.mVelocity = s2.mVelocity.add(impulse.scale(s2.mInvMass));
-        let tangent = relativeVelocity.subtract(n.scale(relativeVelocity.dot(n)));
-            // relativeVelocity.dot(tangent) should less than 0
-        tangent = tangent.normalize().scale(-1);
-        let jT = -(1 + newRestituion) *
-        relativeVelocity.dot(tangent) * newFriction;
-        jT = jT / (s1.mInvMass + s2.mInvMass);
-        // friction should be less than force in normal direction
-        if (jT > jN) jT = jN;
-        //impulse is from s1 to s2 (in opposite direction of velocity)
-        impulse = tangent.scale(jT);
-        s1.mVelocity = s1.mVelocity.subtract(impulse.scale(s1.mInvMass));
-        s2.mVelocity = s2.mVelocity.add(impulse.scale(s2.mInvMass));
+    const resolveCollision = (s1, s2, collisionInfo) => {
+      if (s1.mInvMass === 0 && s2.mInvMass === 0) return;
+
+      if (positionalCorrectionFlag) positionalCorrection(s1, s2, collisionInfo);
+      const n = collisionInfo.getNormal();
+      const v1 = s1.mVelocity;
+      const v2 = s2.mVelocity;
+      const relativeVelocity = v2.subtract(v1);
+      const rVelocityInNormal = relativeVelocity.dot(n);
+
+      if (rVelocityInNormal > 0) return;
+
+      const newRestitution = Math.min(s1.mRestitution, s2.mRestitution);
+      const newFriction = Math.min(s1.mFriction, s2.mFriction);
+
+      let jN = -(1 + newRestitution) * rVelocityInNormal;
+      jN = jN / (s1.mInvMass + s2.mInvMass);
+
+      let impulse = n.scale(jN);
+
+      s1.mVelocity = s1.mVelocity.subtract(impulse.scale(s1.mInvMass));
+      s2.mVelocity = s2.mVelocity.add(impulse.scale(s2.mInvMass));
+
+      let tangent = relativeVelocity.subtract(
+        n.scale(relativeVelocity.dot(n))
+      );
+
+      tangent = tangent.normalize().scale(-1);
+      let jT =
+        -(1 + newRestitution) * relativeVelocity.dot(tangent) * newFriction;
+      jT = jT / (s1.mInvMass + s2.mInvMass);
+
+      if (jT > jN) jT = jN;
+
+      impulse = tangent.scale(jT);
+      s1.mVelocity = s1.mVelocity.subtract(impulse.scale(s1.mInvMass));
+      s2.mVelocity = s2.mVelocity.add(impulse.scale(s2.mInvMass));
     };
 
     for (let k = 0; k < relaxationCount; k++) {
       for (let i = 0; i < allObjectsRef.current.length; i++) {
         for (let j = i + 1; j < allObjectsRef.current.length; j++) {
-          if (allObjectsRef.current[i].boundTest(allObjectsRef.current[j])) {  
-            if (allObjectsRef.current[i].collisionTest(allObjectsRef.current[j], collisionInfo)) {               
-              // Перевірка та зміна напряму нормалі
-              if (collisionInfo.getNormal().dot(allObjectsRef.current[j].mCenter.subtract(allObjectsRef.current[i].mCenter)) < 0) {
-                  collisionInfo.changeDir();
-              } 
-              // Малювання інформації про зіткнення
-              drawCollisionInfo(collisionInfo, context);
-
-              resolveCollision(allObjectsRef.current[i],
+          if (
+            allObjectsRef.current[i].boundTest(allObjectsRef.current[j])
+          ) {
+            if (
+              allObjectsRef.current[i].collisionTest(
                 allObjectsRef.current[j],
-                collisionInfo);
+                collisionInfo
+              )
+            ) {
+              if (
+                collisionInfo
+                  .getNormal()
+                  .dot(
+                    allObjectsRef.current[j].mCenter.subtract(
+                      allObjectsRef.current[i].mCenter
+                    )
+                  ) < 0
+              ) {
+                collisionInfo.changeDir();
+              }
+
+              drawCollisionInfo(collisionInfo, context);
+              resolveCollision(
+                allObjectsRef.current[i],
+                allObjectsRef.current[j],
+                collisionInfo
+              );
             }
           }
         }
-      } 
+      }
     }
   };
 
@@ -120,7 +131,6 @@ const App = () => {
     canvas.width = 800;
     canvas.height = 450;
 
-    // Initial objects setup
     let r1 = new Rectangle(new Vec2(200, 200), 40, 40);
     allObjectsRef.current = [r1];
 
@@ -131,9 +141,7 @@ const App = () => {
 
     runGameLoop();
 
-    // Add new rectangles at random positions every 5 seconds
     const intervalId = setInterval(() => {
-      const canvas = canvasRef.current;
       const width = canvas.width;
       const height = canvas.height;
 
@@ -142,106 +150,17 @@ const App = () => {
 
       let r1 = new Rectangle(new Vec2(randomX, randomY), 40, 40);
       allObjectsRef.current = [...allObjectsRef.current, r1];
-    }, 500);
+    }, 5000);
 
     return () => {
-      clearInterval(intervalId);  // Clean up the interval on component unmount
+      clearInterval(intervalId);
     };
   }, []);
-
-  const handleKeyDown = useCallback((event) => {
-    const keycode = event.keyCode || event.which;
-    const allObjects = allObjectsRef.current;
-    const gObjectNum = gObjectNumRef.current;
-
-    if (keycode === 70) { // F
-      let r1 = new Rectangle(
-        new Vec2(allObjects[gObjectNum].mCenter.x + 200, allObjects[gObjectNum].mCenter.y + 20),
-        Math.random() * 30 + 10,
-        Math.random() * 30 + 10, 200, 200, 1
-      );
-      var r5 = new Rectangle(new Vec2(500, 200), 400, 20, 1, 1, 1);
-    r5.rotate(2.8);
-    var r6 = new Rectangle(new Vec2(200, 400), 400, 20, 20, 1, 0.5);
-      allObjectsRef.current = [...allObjects, r1, r5, r6];
-    }
-
-    if (keycode === 71) { // G
-      let r1 = new Circle(
-        new Vec2(allObjects[gObjectNum].mCenter.x, allObjects[gObjectNum].mCenter.y),
-        Math.random() * 10 + 20
-      );
-      allObjectsRef.current = [...allObjects, r1];
-    }
-
-    if (keycode >= 48 && keycode <= 57) { // Number keys
-      const index = keycode - 48;
-      if (index < allObjects.length) {
-        gObjectNumRef.current = index;
-      }
-    }
-
-    if (keycode === 38) { // Up arrow
-      if (gObjectNumRef.current > 0) {
-        gObjectNumRef.current--;
-      }
-    }
-
-    if (keycode === 40) { // Down arrow
-      if (gObjectNumRef.current < allObjects.length - 1) {
-        gObjectNumRef.current++;
-      }
-    }
-
-    // Move with WASD keys
-    if (keycode === 87) { // W
-      allObjects[gObjectNum].move(new Vec2(0, -10));
-    }
-
-    if (keycode === 83) { // S
-      allObjects[gObjectNum].move(new Vec2(0, 10));
-    }
-
-    if (keycode === 65) { // A
-      allObjects[gObjectNum].move(new Vec2(-10, 0));
-    }
-
-    if (keycode === 68) { // D
-      allObjects[gObjectNum].move(new Vec2(10, 0));
-    }
-
-    // Rotate with QE keys
-    if (keycode === 81) { // Q
-      allObjects[gObjectNum].rotate(-0.1);
-    }
-
-    if (keycode === 69) { // E
-      allObjects[gObjectNum].rotate(0.1);
-    }
-
-    // Toggle gravity with the H key
-    if (keycode === 72) { // H
-      const currentObj = allObjects[gObjectNum];
-      currentObj.mFix = currentObj.mFix === 0 ? 1 : 0;
-    }
-
-    // Reset with the R key
-    if (keycode === 82) { // R
-      allObjectsRef.current.splice(1, allObjects.length - 1); // Keep only the first object
-      gObjectNumRef.current = 0;
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [handleKeyDown]);
 
   return (
     <div>
       <canvas ref={canvasRef} style={{ border: '1px solid black' }}></canvas>
+      <KeyHandler allObjectsRef={allObjectsRef} gObjectNumRef={gObjectNumRef} />
     </div>
   );
 };
--- physics-engine/src/RigidShape.js
+++ physics-engine/src/RigidShape.js
@@ -1,7 +1,7 @@
 import Vec2 from './Vec2';
 
 class RigidShape {
-  constructor(center, mass, friction, restitution) {    
+  constructor(center, mass = 1, friction = 0.8, restitution = 0.2) {
     this.mCenter = center;
     const width = 40;
     const height = 40;
@@ -11,11 +11,8 @@ class RigidShape {
     this.mFix = 0;
 
     this.mVelocity = new Vec2(0, 0);
-    this.mAcceleration = new Vec2(0, 10); // Gravity had to come in here - SV
-    //angle
-    this.mAngle = 0;
-    //negetive-- clockwise
-    //positive-- counterclockwise
+    this.mAcceleration = new Vec2(0, 10); // Gravity
+    this.mAngle = 0; // Angle of rotation
     this.mAngularVelocity = 0;
     this.mAngularAcceleration = 0;
 
@@ -28,67 +25,44 @@ class RigidShape {
     this.mVertex[2] = new Vec2(center.x + width / 2, center.y + height / 2);
     this.mVertex[3] = new Vec2(center.x - width / 2, center.y + height / 2);
 
-    // 0--Top;1--Right;2--Bottom;3--Left
-    // mFaceNormal is normal of face toward outside of rectangle
-    // SV - The face normal vectors will be used later for determining collisions.
-
-    this.mFaceNormal[0] = this.mVertex[1].subtract(this.mVertex[2]);
-    this.mFaceNormal[0] = this.mFaceNormal[0].normalize();
-    this.mFaceNormal[1] = this.mVertex[2].subtract(this.mVertex[3]);
-    this.mFaceNormal[1] = this.mFaceNormal[1].normalize();
-    this.mFaceNormal[2] = this.mVertex[3].subtract(this.mVertex[0]);
-    this.mFaceNormal[2] = this.mFaceNormal[2].normalize();
-    this.mFaceNormal[3] = this.mVertex[0].subtract(this.mVertex[1]);
-    this.mFaceNormal[3] = this.mFaceNormal[3].normalize();
+    // 0--Top; 1--Right; 2--Bottom; 3--Left
+    // mFaceNormal is the normal of the face towards the outside of the rectangle
+    this.mFaceNormal[0] = this.mVertex[1].subtract(this.mVertex[2]).normalize();
+    this.mFaceNormal[1] = this.mVertex[2].subtract(this.mVertex[3]).normalize();
+    this.mFaceNormal[2] = this.mVertex[3].subtract(this.mVertex[0]).normalize();
+    this.mFaceNormal[3] = this.mVertex[0].subtract(this.mVertex[1]).normalize();
 
     this.mCenter = center;
     this.mInertia = 0;
-    if (mass !== undefined)
-    this.mInvMass = mass;
-    else
-    this.mInvMass = 1;
-    if (friction !== undefined)
-    this.mFriction = friction;
-    else
-    this.mFriction = 0.8;
-    if (restitution !== undefined)
-    this.mRestitution = restitution;
-    else
 
-    this.mRestitution = 0.2;
- this.mVelocity = new Vec2(0, 0);
- if (this.mInvMass !== 0) {
- this.mInvMass = 1 / this.mInvMass;
- this.mAcceleration = new Vec2(0, 10);
- } else {
- this.mAcceleration = new Vec2(0, 0);
- }
- //angle
- this.mAngle = 0;
- //negetive-- clockwise
- //positive-- counterclockwise
- this.mAngularVelocity = 0;
- this.mAngularAcceleration = 0;
- this.mBoundRadius = 0;
+    this.mInvMass = mass !== undefined ? mass : 1;
+    this.mFriction = friction !== undefined ? friction : 0.8;
+    this.mRestitution = restitution !== undefined ? restitution : 0.2;
 
- this.updateInertia();
+    this.mVelocity = new Vec2(0, 0);
+    if (this.mInvMass !== 0) {
+      this.mInvMass = 1 / this.mInvMass;
+      this.mAcceleration = new Vec2(0, 10);
+    } else {
+      this.mAcceleration = new Vec2(0, 0);
+    }
+
+    this.mAngle = 0;
+    this.mAngularVelocity = 0;
+    this.mAngularAcceleration = 0;
+    this.mBoundRadius = 0;
+
+    this.updateInertia();
   }
 
-  update(canvasDimensions) {    
-    // if (canvasDimensions && this.mCenter.y < canvasDimensions.height && this.mFix !== 0) {
-    //   //this.move(new Vec2(0, .1));
-    //   //this.rotate(.01);
-    // }
-    if (false) { // mMovement comes in - SV
-      //var dt = gEngine.Core.mUpdateIntervalInSeconds;
+  update(canvasDimensions) {
+    if (false) {
       const dt = 200;
-      //v += a*t
       this.mVelocity = this.mVelocity.add(this.mAcceleration.scale(dt));
-      //s += v*t
       this.move(this.mVelocity.scale(dt));
       this.mAngularVelocity += this.mAngularAcceleration * dt;
       this.rotate(this.mAngularVelocity * dt);
-      }
+    }
   }
 
   move(vec) {
@@ -99,50 +73,40 @@ class RigidShape {
     this.mAngle += angle;
   }
 
-  boundTest (otherShape) {    
-    var vFrom1to2 = otherShape.mCenter.subtract(this.mCenter);
-    var rSum = this.mBoundRadius + otherShape.mBoundRadius;
-    var dist = vFrom1to2.length();
+  boundTest(otherShape) {
+    const vFrom1to2 = otherShape.mCenter.subtract(this.mCenter);
+    const rSum = this.mBoundRadius + otherShape.mBoundRadius;
+    const dist = vFrom1to2.length();
 
-    if (dist > rSum) {
-       return false; //not overlapping
+    return dist <= rSum;
+  }
+
+  updateMass(delta) {
+    let mass = this.mInvMass !== 0 ? 1 / this.mInvMass : 0;
+    mass += delta;
+
+    if (mass <= 0) {
+      this.mInvMass = 0;
+      this.mVelocity = new Vec2(0, 0);
+      this.mAcceleration = new Vec2(0, 0);
+      this.mAngularVelocity = 0;
+      this.mAngularAcceleration = 0;
+    } else {
+      this.mInvMass = 1 / mass;
+      this.mAcceleration = new Vec2(0, 10);
     }
 
-    
-    
-    return true;
-};
-updateMass (delta) {
-  var mass;
-  if (this.mInvMass !== 0)
-  mass = 1 / this.mInvMass;
-  else
-  mass = 0;
-  mass += delta;
-  if (mass <= 0) {
-  this.mInvMass = 0;
-  this.mVelocity = new Vec2(0, 0);
-  this.mAcceleration = new Vec2(0, 0);
-  this.mAngularVelocity = 0;
-  this.mAngularAcceleration = 0;
-  } else {
-  this.mInvMass = 1 / mass;
-  this.mAcceleration = new Vec2(0, 10);
+    this.updateInertia();
   }
-  this.updateInertia();
-}
 
-updateInertia = function () {
-      // Expect this.mInvMass to be already inverted!
- if (this.mInvMass === 0)
-  this.mInertia = 0;
-  else {
-  //inertia=mass*width^2+height^2
-  this.mInertia = (1 / this.mInvMass) * (this.mWidth *
- this.mWidth + this.mHeight * this.mHeight) / 12;
-  this.mInertia = 1 / this.mInertia;
+  updateInertia() {
+    if (this.mInvMass === 0) {
+      this.mInertia = 0;
+    } else {
+      this.mInertia = (1 / this.mInvMass) * (this.width ** 2 + this.height ** 2) / 12;
+      this.mInertia = 1 / this.mInertia;
+    }
   }
- };
 }
 
 export default RigidShape;
